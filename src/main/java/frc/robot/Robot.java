25e00329ae93b8cbd3838f66b86c9b5da4fed9d1
Yoav Schreiber <79276554+Takamagahara@users.noreply.github.com>
2022-12-11 16:48:13 +0200

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,9 +16,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -35,16 +38,18 @@ public class Robot extends TimedRobot {
 
   RelativeEncoder encoder;
   double currentArmAngle;
-  
 
   double currentVoltage, currentVelocity;
+  double exportedValue;
 
+  double kA = 0.0;
   double kG = 0.65;
   double kS = 0.12;
-
+  double kV = 49.52; 
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
@@ -65,46 +70,59 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("voltage", 0);
     SmartDashboard.putNumber("armAngle", 0);
     SmartDashboard.putNumber("currentVelocity", 0);
+    SmartDashboard.putNumber("exportedValue", 0);
 
     forwardLimitSwitch.enableLimitSwitch(true);
     reverseLimitSwitch.enableLimitSwitch(true);
 
     encoder = mainSM.getEncoder();
     encoder.setPositionConversionFactor(360.0 / 36.6);
-    encoder.setVelocityConversionFactor(0.1047); // Xrev/m -> rad/s (=(X*2PI) / 60s)
+    encoder.setVelocityConversionFactor(0.1047); // Xrev/m -> rad/s (=(X*2PI) / 60s) 
 
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
     currentVoltage = SmartDashboard.getNumber("voltage", 0);
-    currentVelocity = encoder.getVelocity(); 
+    currentVelocity = encoder.getVelocity();
     SmartDashboard.putNumber("currentVelocity", currentVelocity);
-    SmartDashboard.putNumber("voltage", currentVoltage);  
-    
+    SmartDashboard.putNumber("voltage", currentVoltage);
+    exportedValue = currentVoltage / currentVelocity;
 
-    SmartDashboard.putNumber("armAngle", encoder.getPosition()); // check: position to angle? 
+
+    currentVoltage = (forwardLimitSwitch.isPressed() || reverseLimitSwitch.isPressed()) ? 0 : currentVoltage;
+
+    SmartDashboard.putNumber("armAngle", encoder.getPosition()); // check: position to angle?
     SmartDashboard.putBoolean("forward ls pressed", forwardLimitSwitch.isPressed());
     SmartDashboard.putBoolean("reverse ls pressed", reverseLimitSwitch.isPressed());
-    
 
   }
 
   /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
    *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
    * chooser code above as well.
    */
   @Override
@@ -130,7 +148,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   /** This function is called periodically during operator control. */
   @Override
@@ -140,25 +159,31 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 } //
